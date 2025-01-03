982ff27f2fda676bffba3519567639c9e1178790
Indhu0525 <indu01401@hmail.com>
2025-01-03 19:54:47 +0530

    message...

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <h1 className="text-3xl font-bold underline underline-offset-1 text-white">
+  react
+  </h1>
+    </div>
+  );
+}
+
+export default App;
